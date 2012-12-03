56f13c52289323b8f4154896fef8fa4d9f1b35f8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-12-03 08:42:40 +0100

    initial structure
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ autogen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+autoreconf -i -Wall
+intltoolize --force --automake
+
---
+++ man/1.mk
@@ -0,0 +1,21 @@
+# This file is part of facron.
+#
+# Copyright 2012 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+#
+# facron is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# facron is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with facron.  If not, see <http://www.gnu.org/licenses/>.
+
+dist_man_MANS = \
+	man/1/facron.1 \
+	$(NULL)
+
