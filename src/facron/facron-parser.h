a9df346aa3d63b25d96362afcc235ec41a376c8e
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-09-10 13:35:59 +0200

    update copyrights headers
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/facron/facron-parser.h
+++ src/facron/facron-parser.h
@@ -1,7 +1,7 @@
 /*
  *      This file is part of facron.
  *
- *      Copyright 2012 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+ *      Copyright 2012-2015 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
  *
  *      facron is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
